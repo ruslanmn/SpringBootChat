7ceca09e24e24166b858078a113d00a92705dad3
MurRN <R.Murtazin@bssys.com>
2020-09-13 03:35:50 +0300

    Simple user login

--- src/main/java/com/github/ruslanmn/springbootchat/fake/FakeRest.java
+++ src/main/java/com/github/ruslanmn/springbootchat/fake/FakeRest.java
@@ -1,14 +1,21 @@
 package com.github.ruslanmn.springbootchat.fake;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/fake")
-public class Fake {
-    @GetMapping
+public class FakeRest {
+
+    @GetMapping("/fakerest")
     public String helloWorld() {
         return "Hello world";
+    }
+
+    @GetMapping("/chat")
+    public String chat(Authentication authentication) {
+        return "Hello, " + authentication.getName();
     }
 }
