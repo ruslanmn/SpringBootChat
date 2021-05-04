27dff079bc060a29a8c0727dec56459f0fc2000b
19079884 <rnmurtazin@sberbank.ru>
2021-05-05 03:28:32 +0400

    Refactor: removed lombok and fake controller

--- src/main/kotlin/com/github/ruslanmn/springbootchat/controllers/ChatController.kt
+++ src/main/kotlin/com/github/ruslanmn/springbootchat/controllers/ChatController.kt
@@ -1,27 +1,11 @@
 package com.github.ruslanmn.springbootchat.controllers
 
 import com.github.ruslanmn.springbootchat.controllers.dto.Message
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
-import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.beans.factory.annotation.Autowired
-import kotlin.Throws
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.crypto.password.PasswordEncoder
-import lombok.AllArgsConstructor
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.web.util.HtmlUtils
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import kotlin.jvm.JvmStatic
-import org.springframework.boot.SpringApplication
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
+import org.springframework.web.util.HtmlUtils
 
 @Controller
 class ChatController {
--- src/main/kotlin/com/github/ruslanmn/springbootchat/controllers/dto/Message.kt
+++ src/main/kotlin/com/github/ruslanmn/springbootchat/controllers/dto/Message.kt
@@ -1,9 +1,5 @@
 package com.github.ruslanmn.springbootchat.controllers.dto
 
-import lombok.AllArgsConstructor
-import lombok.Getter
-import lombok.Setter
-
 data class Message (
     val username: String,
     val text: String
